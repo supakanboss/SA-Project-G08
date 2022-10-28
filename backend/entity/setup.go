c7ee30f7b9f081e5a0391ec713235459f3f915b9
cutekaennie <B6332426@g.sut.ac.th>
2022-10-28 14:45:30 +0700

    first commit

--- backend/entity/setup.go
+++ backend/entity/setup.go
@@ -32,19 +32,104 @@ func SetupDatabase() {
 
 	/////////////ข้อมูลใน entity LOCATION///////////
 	Location1 := LOCATION{
-		Location_Name: "สนามฟุตบอล",
+		Location_Name: "สนาม ฟุตบอล 1",
 	}
 	db.Model(&LOCATION{}).Create(&Location1)
 
 	Location2 := LOCATION{
-		Location_Name: "สระว่ายน้ำ",
+		Location_Name: "สนาม ฟุตบอล 2",
 	}
 	db.Model(&LOCATION{}).Create(&Location2)
 
 	Location3 := LOCATION{
-		Location_Name: "สนามแบดมินตัน",
+		Location_Name: "สระว่ายน้ำ",
 	}
 	db.Model(&LOCATION{}).Create(&Location3)
+
+	Location4 := LOCATION{
+		Location_Name: "สนาม เทนนิส 1",
+	}
+	db.Model(&LOCATION{}).Create(&Location4)
+
+	Location5 := LOCATION{
+		Location_Name: "สนาม เทนนิส 2",
+	}
+	db.Model(&LOCATION{}).Create(&Location5)
+
+	Location6 := LOCATION{
+		Location_Name: "สนาม วอลเลย์บอล 1",
+	}
+	db.Model(&LOCATION{}).Create(&Location6)
+
+	Location7 := LOCATION{
+		Location_Name: "สนาม วอลเลย์บอล 2",
+	}
+	db.Model(&LOCATION{}).Create(&Location7)
+
+	Location8 := LOCATION{
+		Location_Name: "สนาม เปตอง 1",
+	}
+	db.Model(&LOCATION{}).Create(&Location8)
+
+	Location9 := LOCATION{
+		Location_Name: "สนาม เปตอง 2",
+	}
+	db.Model(&LOCATION{}).Create(&Location9)
+
+	Location10 := LOCATION{
+		Location_Name: "สนาม บาสเกตบอล 1",
+	}
+	db.Model(&LOCATION{}).Create(&Location10)
+
+	Location11 := LOCATION{
+		Location_Name: "สนาม บาสเกตบอล 2",
+	}
+	db.Model(&LOCATION{}).Create(&Location11)
+
+	Location12 := LOCATION{
+		Location_Name: "สนาม มวย",
+	}
+	db.Model(&LOCATION{}).Create(&Location12)
+
+	Location13 := LOCATION{
+		Location_Name: "สนาม ฟุตซอล 1",
+	}
+	db.Model(&LOCATION{}).Create(&Location13)
+
+	Location14 := LOCATION{
+		Location_Name: "สนาม ฟุตซอล 2",
+	}
+	db.Model(&LOCATION{}).Create(&Location14)
+
+	Location15 := LOCATION{
+		Location_Name: "โรงยิม (คอร์ดแบดมินตัน1)",
+	}
+	db.Model(&LOCATION{}).Create(&Location15)
+
+	Location16 := LOCATION{
+		Location_Name: "โรงยิม (คอร์ดแบดมินตัน2)",
+	}
+	db.Model(&LOCATION{}).Create(&Location16)
+
+	Location17 := LOCATION{
+		Location_Name: "โรงยิม (คอร์ดแบดมินตัน3)",
+	}
+	db.Model(&LOCATION{}).Create(&Location17)
+
+	Location18 := LOCATION{
+		Location_Name: "โรงยิม (คอร์ดแบดมินตัน4)",
+	}
+	db.Model(&LOCATION{}).Create(&Location18)
+
+	Location19 := LOCATION{
+		Location_Name: "โรงยิม (คอร์ดแบดมินตัน5)",
+	}
+	db.Model(&LOCATION{}).Create(&Location19)
+
+	Location20 := LOCATION{
+		Location_Name: "โรงยิม (คอร์ดแบดมินตัน6)",
+	}
+	db.Model(&LOCATION{}).Create(&Location20)
 
 	////////////ข้อมูลใน entity SPORT_TYPE/////////
 	Sport_type1 := SPORT_TYPE{
@@ -58,46 +143,59 @@ func SetupDatabase() {
 	db.Model(&SPORT_TYPE{}).Create(&Sport_type2)
 
 	Sport_type3 := SPORT_TYPE{
-		Sport_Type_Name: "แบดมินตัน",
+		Sport_Type_Name: "เทนนิส",
 	}
 	db.Model(&SPORT_TYPE{}).Create(&Sport_type3)
 
-	//ข้อมํูลในตาราง sport epuipment tpye
+	Sport_type4 := SPORT_TYPE{
+		Sport_Type_Name: "วอลเลย์บอล",
+	}
+	db.Model(&SPORT_TYPE{}).Create(&Sport_type4)
+
+	Sport_type5 := SPORT_TYPE{
+		Sport_Type_Name: "เปตอง",
+	}
+	db.Model(&SPORT_TYPE{}).Create(&Sport_type5)
+
+	Sport_type6 := SPORT_TYPE{
+		Sport_Type_Name: "บาสเกตบอล",
+	}
+	db.Model(&SPORT_TYPE{}).Create(&Sport_type6)
+
+	Sport_type7 := SPORT_TYPE{
+		Sport_Type_Name: "มวย",
+	}
+	db.Model(&SPORT_TYPE{}).Create(&Sport_type7)
+
+	Sport_type8 := SPORT_TYPE{
+		Sport_Type_Name: "ฟุตซอล",
+	}
+	db.Model(&SPORT_TYPE{}).Create(&Sport_type8)
+
+	////////ข้อมํูลในตาราง sport epuipment type///////////
 	Sport_Equipment_Type1 := SPORT_EQUIPMENT_TYPE{
-		SPORT_EQUIPMENT_TYPE_Name: "รองเท้า",
+		SPORT_EQUIPMENT_TYPE_Name: "ประเภท รองเท้า",
 	}
 	db.Model(&SPORT_EQUIPMENT_TYPE{}).Create(&Sport_Equipment_Type1)
 
 	Sport_Equipment_Type2 := SPORT_EQUIPMENT_TYPE{
-		SPORT_EQUIPMENT_TYPE_Name: "ลูก",
+		SPORT_EQUIPMENT_TYPE_Name: "ประเภท ไม้",
 	}
 	db.Model(&SPORT_EQUIPMENT_TYPE{}).Create(&Sport_Equipment_Type2)
 
 	Sport_Equipment_Type3 := SPORT_EQUIPMENT_TYPE{
-		SPORT_EQUIPMENT_TYPE_Name: "ไม้",
+		SPORT_EQUIPMENT_TYPE_Name: "ประเภท ลูก",
 	}
 	db.Model(&SPORT_EQUIPMENT_TYPE{}).Create(&Sport_Equipment_Type3)
 
-	//ข้อมูลตารางreport ตารางหลัก
-	db.Model(&REPORT{}).Create(&REPORT{
-		Location:             Location3,
-		Sport_Equipment_Type: Sport_Equipment_Type3,
-		Sport_Type:           Sport_type3,
-		Detail:               "หนูตบแรงจนไม้แบดมินตันหัก",
-	})
+	Sport_Equipment_Type4 := SPORT_EQUIPMENT_TYPE{
+		SPORT_EQUIPMENT_TYPE_Name: "เสื้อวอร์ม",
+	}
+	db.Model(&SPORT_EQUIPMENT_TYPE{}).Create(&Sport_Equipment_Type4)
 
-	db.Model(&REPORT{}).Create(&REPORT{
-		Location:             Location1,
-		Sport_Equipment_Type: Sport_Equipment_Type1,
-		Sport_Type:           Sport_type1,
-		Detail:               "ผมเตะลูกฟุตบอลแตกครับ",
-	})
-
-	db.Model(&REPORT{}).Create(&REPORT{
-		Location:             Location3,
-		Sport_Equipment_Type: Sport_Equipment_Type2,
-		Sport_Type:           Sport_type3,
-		Detail:               "ลูกแบดมินตันพังค่ะ",
-	})
+	Sport_Equipment_Type5 := SPORT_EQUIPMENT_TYPE{
+		SPORT_EQUIPMENT_TYPE_Name: "อุปกรณ์อื่นๆ",
+	}
+	db.Model(&SPORT_EQUIPMENT_TYPE{}).Create(&Sport_Equipment_Type5)
 
 }
